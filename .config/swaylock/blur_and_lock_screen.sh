076beb8b4f9893a806193aa8197540e40dca10d7
XiaowenHu96 <xihu5895@uni.sydney.edu.au>
2023-09-11 12:22:31 +0800

    switch to swaylock+

--- .config/swaylock/blur_and_lock_screen.sh
+++ .config/swaylock/blur_and_lock_screen.sh
@@ -2,6 +2,31 @@
 
 ROOT=~/.config/swaylock/
 # Modify if multiple monitors are used 
-grim $ROOT/screenshot_0.png
-convert $ROOT/screenshot_0.png -filter Gaussian -blur 0x5 $ROOT/screenshot.png
-swaylock -f -i $ROOT/screenshot.png --indicator-radius 60
+# grim $ROOT/screenshot_0.png
+# convert $ROOT/screenshot_0.png -filter Gaussian -blur 0x5 $ROOT/screenshot.png
+# swaylock -f -i $ROOT/screenshot.png --indicator-radius 60
+
+# This is swaylock-effect-git, has built-in blur feature and clock
+swaylock \
+--screenshots \
+--indicator \
+--clock \
+--inside-wrong-color fb4934 \
+--line-wrong-color fb4934 \
+--ring-wrong-color fb4934  \
+--inside-clear-color 8ec07c \
+--ring-clear-color 8ec07c \
+--line-clear-color 8ec07c \
+--inside-ver-color 83a598 \
+--ring-ver-color 83a598 \
+--line-ver-color 83a598  \
+--text-color  fbf1c7 \
+--indicator-radius 80 \
+--indicator-thickness 16 \
+--effect-blur 9x9 \
+--ring-color 32302f \
+--key-hl-color d3869b \
+--line-color 32302f \
+--inside-color 32302f \
+--separator-color 00000000 \
+--fade-in 0.15 &
